4031be9380efdef3c018fe8195ac553e783c1372
silveira.jedi@gmail.com <P4bm@64lni!!2020>
2022-03-14 22:40:31 -0300

    Vercionando o Gerenciador de Tarefas

--- Exemplos/Tarefas/index.php
+++ Exemplos/Tarefas/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gerenciador de Tarefas</title>
+    <title>Gerenciador de Tarefas - 1.0</title>
     <style>
         body {
             font-family: Sans-serif;
